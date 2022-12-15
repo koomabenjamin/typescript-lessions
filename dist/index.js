1d42493df649432c719bdde9159e5c5298b92244
User <user@Users-iMac.local>
2022-12-15 16:06:54 +0300

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const message = "we are going to fetch patients here.";
+fetchPatients(message);
+function fetchPatients(message) {
+    console.log(message);
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// console.log('Typescript upgrade happening right now.')
+const message: string = "we are going to fetch patients here.";
+
+fetchPatients(message);
+
+function fetchPatients(message: string): void{
+  console.log(message);
+  // return message;
+}
+
+// ====== TYPES ===========
+// ====== INTERFACES ===========
+// ====== FUNCTIONS ===========
+// ====== CLASSES ===========
+// ====== DECORATORS ===========
+// ====== DECLARATIONS ===========
